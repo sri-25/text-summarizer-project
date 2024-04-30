2b37b0c8364c66e45836b0d454d1b8dffa382d7d
Srijan Gupta <srijansmac@Srijans-MacBook-Pro.local>
2024-04-29 21:43:42 -0400

    added data transformation code

---
+++ src/textsummarizer/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,14 @@
+from textsummarizer.config.configuration import ConfigurationManager
+from textsummarizer.components.data_transformation import DataTransformation
+from textsummarizer.logging import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
