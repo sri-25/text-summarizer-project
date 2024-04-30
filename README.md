402c7cdb80979ccdc671ce349a23289e38c6aa89
Srijan Gupta <srijansmac@Srijans-MacBook-Pro.local>
2024-04-29 21:04:36 -0400

    data ingestion performed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# text-summarizer-project
+# End to end text-summarizer-project
+
+## workflows
+
+1. update config.yaml file
+2. update params.yaml file
+3. update entity
+4. update the configuration manager in src config
+5. update the components
+6. update the pipeline
+7. update the main.py
+8. update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from textsummarizer.logging import logger 
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.logging import logger
 
-logger.info("Welcome to our custom logger!")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
